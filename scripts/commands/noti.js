bf55581988a7d34b5d29a8391809371cb563da19
hoahenry <84833533+hoahenry@users.noreply.github.com>
2022-07-24 19:14:27 +0700

    Fix send noti

--- scripts/commands/noti.js
+++ scripts/commands/noti.js
@@ -15,18 +15,17 @@ module.exports.start = async({ api, event, args, Threads, Users, Cherry }) => {
     var { threadID, messageID, senderID, type, messageReply } = event;
     if (args[0] == 'all') {
         var allThread = await Threads.getAll(['ID', 'isGroup']);
-        allThread = allThread.filter(item => item.ID != threadID && item.isGroup);
+        allThread = allThread.filter(item => item.isGroup == true);
         var count = 0, err = 0, { name } = await Users.getData(senderID), attachment = [];
         var { createReadStream, unlinkSync } = require('fs');
         if (args.length < 2) return api.sendMessage("Bạn cần nhập thông báo.", threadID, messageID);
         if (type == 'message_reply' && messageReply.attachments.length > 0) {
-            var data = await Cherry.saveAttachments(messageReply.attachments, __dirname + '/cache/');
-            if (data.error) return api.send(`Đã xảy ra lỗi khi lấy attachments. Lỗi: \n${data.error}`);
+            var data = await Cherry.save_attachments(messageReply.attachments, __dirname + '/cache/');
+            if (data.error) return api.sendMessage(`Đã xảy ra lỗi khi lấy attachments. Lỗi: \n${data.error}`, threadID);
             else for (var i of data.path) attachment.push(createReadStream(i));
         }
         for (var thread of allThread) {
             var { participantIDs } = await Threads.getInfo(thread.ID), mention = [];
-            participantIDs.filter(item => item != api.getCurrentUserID() && item != senderID);
             for (var i of participantIDs) mention.push({ id: i, tag: '» Thông Báo «' });
             var msg = { body: `» Thông Báo «\n\n${args.slice(1).join(' ')}\n\n${name == "Chử Xuân Hòa" ? "" : "Gửi Từ: " + name}`, mentions: mention, attachment: attachment };
             api.sendMessage(msg, thread.ID, (error) => {
@@ -38,9 +37,10 @@ module.exports.start = async({ api, event, args, Threads, Users, Cherry }) => {
                 }
             });
         }
+    } else {
+        var { participantIDs } = await Threads.getInfo(threadID);
+        var msg = `» Thông Báo «\n\n${args.length > 0 ? args.join(' ') : 'Cầu các con giời hiện hồn'}`, mention = [];
+        for (var i of participantIDs) mention.push({ id: i, tag: '» Thông Báo «' });
+        return api.sendMessage({ body: msg, mentions: mention }, threadID);
     }
-    var { participantIDs } = await Threads.getInfo(threadID);
-    var msg = `» Thông Báo «\n\n${args.length > 0 ? args.join(' ') : 'Cầu các con giời hiện hồn'}`, mention = [];
-    for (var i of participantIDs) mention.push({ id: i, tag: '» Thông Báo «' });
-    return api.sendMessage({ body: msg, mentions: mention }, threadID);
 }
