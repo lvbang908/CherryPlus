44d9e6476d6abede5ede70005acc94212da991d8
hoahenry <84833533+hoahenry@users.noreply.github.com>
2022-07-29 01:13:18 +0700

    Update language

--- scripts/commands/unsend.js
+++ scripts/commands/unsend.js
@@ -11,13 +11,24 @@ module.exports.info = {
 	countdown: 5
 };
 
+module.exports.language = {
+	vi: {
+		reply: 'Hãy reply tin nhắn cần gỡ.',
+		author: 'Không thể gỡ tin nhắn của người khác.'
+	},
+	en: {
+		reply: 'Please reply to the message to be removed.',
+		author: 'Cannot remove other people\'s messages.'
+	}
+}
+
 module.exports.handleEvents = function({ api, event }) {
 	var { body, type } = event;
 	if (type == "message_reply" && body == "." && event.messageReply.senderID == api.getCurrentUserID()) return api.unsendMessage(event.messageReply.messageID);
 }
 
-module.exports.start = function({ api, event }) {
-	if (event.type != "message_reply") return api.sendMessage('Hãy reply tin nhắn cần gỡ.', event.threadID, event.messageID);
-	if (event.messageReply.senderID != api.getCurrentUserID()) return api.sendMessage('Không thể gỡ tin nhắn của người khác.', event.threadID, event.messageID);
+module.exports.start = function({ api, event, Language }) {
+	if (event.type != "message_reply") return api.sendMessage(Language.get('unsend', 'reply'), event.threadID, event.messageID);
+	if (event.messageReply.senderID != api.getCurrentUserID()) return api.sendMessage(Language.get('unsend', 'author'), event.threadID, event.messageID);
 	return api.unsendMessage(event.messageReply.messageID);
 }
